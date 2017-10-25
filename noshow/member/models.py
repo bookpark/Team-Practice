ac9c8d09185509f63a85fa530fc8c9ec5bfd34c3
Booki Park <bkbkgg@gmail.com>
2017-10-25 14:33:36 +0900

    Created user model

--- noshow/member/models.py
+++ noshow/member/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=20)
+    dob = models.DateField(
+        '생년월일',
+        max_length=8,
+    )
+    cp = models.IntegerField(
+        '휴대전화',
+        max_length=11,
+    )
