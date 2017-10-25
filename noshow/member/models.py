2524418bf1e84c625cbff3b2eaf5db60d130fb88
Booki Park <bkbkgg@gmail.com>
2017-10-25 15:06:53 +0900

    Modified user model

--- noshow/member/models.py
+++ noshow/member/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20)
     dob = models.DateField(
         '생년월일',
         max_length=8,
     )
     cp = models.IntegerField(
         '휴대전화',
-        max_length=11,
     )
